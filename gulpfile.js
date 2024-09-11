fda0fbe63ee22591075f86bc1435df65b968b120
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2024-09-11 12:30:02 -0600

    ADD compilation function for sass to gulpfile.js and ADD font to index.html

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+import {src, dest, watch} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+// Compiling SASS with the gulp-sass dependency
+const sass = gulpSass(dartSass);
+
+export function css(done) {
+    // sass file location
+    src('src/scss/app.scss')
+        // call the compilation function
+        .pipe( sass() )
+        // css storage destination
+        .pipe( dest('build/css') )
+
+    done()
+}
+
+// monitoring files for compile
+export function dev() {
+    watch('src/scss/app.scss', css)
+}
