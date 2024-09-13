fc5f294c70e2cfc0519c8b4b2f1c0772ec03fe48
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2024-09-13 13:51:41 -0600

    CREATE js folder with app.js and ADD js compile function to gulpfile.js, linking to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
-import {src, dest, watch} from 'gulp'
+import {src, dest, watch, series} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 // Compiling SASS with the gulp-sass dependency
 const sass = gulpSass(dartSass);
+
+export function js(done) {
+
+    src('src/js/app.js')
+        .pipe(dest('build/js'))
+
+    done()
+}
 
 export function css(done) {
     // sass file location
@@ -19,4 +27,10 @@ export function css(done) {
 // monitoring files for compile
 export function dev() {
     watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
 }
+
+// run multiple tasks whitout a name (it is no need a name to run in package.json)
+// serries run function one after another
+// parallel run all at the same time
+export default series(js, css, dev)
