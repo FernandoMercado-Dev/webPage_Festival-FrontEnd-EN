848c5a69aeafb81c9aab73088f3d7e6beeff3399
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2024-09-13 19:53:21 -0600

    CREATE _gallery.scss for responsive styling of gallery section with modal for selected images and ADD overflow-hidden modifier to body in _global.scss

--- build/js/app.js
+++ build/js/app.js
@@ -9,9 +9,52 @@ function createGallery() {
 
     for(let i = 1; i <= IMAGE_COUNT; i++) {
         const image = document.createElement('IMG');
-            image.src = `src/img/gallery/full/${i}.jpg`;
-            image.alt = 'Gallery Image';
+        image.src = `src/img/gallery/full/${i}.jpg`;
+        image.alt = 'Gallery Image';
+
+        // Event Handler
+        image.onclick = function() {
+            showImage(i);
+        }
 
         gallery.appendChild(image);
     }
+}
+
+function showImage(i) {
+    const image = document.createElement('IMG');
+    image.src = `src/img/gallery/full/${i}.jpg`;
+    image.alt = 'Gallery Image';
+
+    // Create Modal
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal');
+    modal.onclick = closeModal
+
+    // Modal Close Button
+    const modalCloseBtn =document.createElement('BUTTON');
+    modalCloseBtn.textContent = 'X';
+    modalCloseBtn.classList.add('btn-close');
+    modalCloseBtn.onclick = closeModal
+    
+    // Add image and button on modal
+    modal.appendChild(image);
+    modal.appendChild(modalCloseBtn);
+
+    // Add to html
+    const body = document.querySelector('body');
+    body.classList.add('overflow-hidden');
+    body.appendChild(modal);
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out')
+
+    setTimeout(() => {
+        modal?.remove();
+
+        const body = document.querySelector('body');
+        body.classList.remove('overflow-hidden');
+    }, 500)
 }
